ed0a29d5018c6aeafd05a1e6b125030605c38708
Colin Woodbury <colin@fosskers.ca>
2024-10-18 00:02:14 +0900

    [game] game loop uses tail recursion
    This ensures that my live-updates are actually recognized by the game.
    If I use `loop` or `labels` instead, then the game stays in that loop in
    the previous definition of the function, even if I change it.

--- src/package.lisp
+++ src/package.lisp
@@ -6,36 +6,55 @@
 (defparameter +screen-width+ (* 256 3))
 (defparameter +screen-height+ (* 240 3))
 
-(defun event-loop ()
-  (let* ((sprite (raylib:load-texture "/home/colin/code/common-lisp/save-the-farm/data/sprites/farmer.png"))
-         (pos    (raylib:make-vector2 :x 0.0 :y 0.0))
-         (bbox   (raylib:make-rectangle :x 0.0 :y 0.0
-                                        :width (/ (raylib:texture-width sprite) 7.0)
-                                        :height (float (raylib:texture-height sprite))))
-         (center-x (/ +screen-width+ 2.0))
+(defstruct (sprite (:copier nil) (:predicate nil))
+  (texture nil :read-only t)
+  (pos (raylib:make-vector2 :x 0.0 :y 0.0))
+  (bbox nil :read-only t))
+
+(defun draw-sprite (sprite)
+  (raylib:draw-texture-rec (sprite-texture sprite) (sprite-bbox sprite) (sprite-pos sprite) raylib:+white+))
+
+(defun event-loop-inner (camera sprite fc)
+  (unless (raylib:window-should-close)
+    (raylib:begin-drawing)
+    (raylib:begin-mode-2d camera)
+    (raylib:clear-background raylib:+raywhite+)
+    (raylib:draw-rectangle 0 0 10 10 raylib:+red+)
+    (raylib:draw-pixel 0 0 raylib:+blue+)
+    (draw-sprite sprite)
+    (raylib:end-mode-2d)
+    (raylib:draw-fps 10 10)
+    (raylib:draw-text (format nil "FC: ~a" fc) 10 (- +screen-height+ 25) 20 raylib:+lightgray+)
+    (raylib:end-drawing)
+    (incf fc)
+    (event-loop-inner camera sprite fc)))
+
+(defun event-loop (camera)
+  (let ((sprite (make-farmer)))
+    (event-loop-inner camera sprite 0)
+    (raylib:unload-texture (sprite-texture sprite))))
+
+(defun init-camera ()
+  "Produce a 2D Camera."
+  (let* ((center-x (/ +screen-width+ 2.0))
          (center-y (/ +screen-height+ 2.0))
          (offset   (raylib:make-vector2 :x center-x :y center-y))
-         (target   (raylib:make-vector2 :x 0.0 :y 0.0))
-         (camera   (raylib:make-camera-2d :offset offset :target target :rotation 0.0 :zoom 3.0)))
-    (labels ((recurse ()
-               (unless (raylib:window-should-close)
-                 (raylib:begin-drawing)
-                 (raylib:begin-mode-2d camera)
-                 (raylib:clear-background raylib:+raywhite+)
-                 (raylib:draw-rectangle 0 0 10 10 raylib:+red+)
-                 (raylib:draw-pixel 0 0 raylib:+blue+)
-                 (raylib:draw-texture-rec sprite bbox pos raylib:+white+)
-                 (raylib:end-mode-2d)
-                 (raylib:end-drawing)
-                 (recurse))))
-      (recurse)
-      (raylib:unload-texture sprite))))
+         (target   (raylib:make-vector2 :x 0.0 :y 0.0)))
+    (raylib:make-camera-2d :offset offset :target target :rotation 0.0 :zoom 3.0)))
+
+(defun make-farmer ()
+  "Read in the farmer texture."
+  (let* ((sprite (raylib:load-texture "/home/colin/code/common-lisp/save-the-farm/data/sprites/farmer.png"))
+         (bbox   (raylib:make-rectangle :x 0.0 :y 0.0
+                                        :width (/ (raylib:texture-width sprite) 7.0)
+                                        :height (float (raylib:texture-height sprite)))))
+    (make-sprite :texture sprite :bbox bbox)))
 
 (defun launch ()
   "Launch the game."
   (raylib:init-window +screen-width+ +screen-height+ "raylib/CL Example")
   (raylib:set-target-fps 60)
-  (event-loop)
+  (event-loop (init-camera))
   (raylib:close-window))
 
 #+nil
