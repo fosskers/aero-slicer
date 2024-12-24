73ee3565fa950e5584545887cc98a77a03ed9387
Colin Woodbury <colin@fosskers.ca>
2024-12-24 12:51:45 +0900

    [deps] delete dead deps

--- vendored/cffi-ops/package.lisp
+++
@@ -1,7 +0,0 @@
-(defpackage cffi-ops
-  (:use #:cl #:alexandria #:arrow-macros #:cffi #:trivial-macroexpand-all)
-  (:import-from #:cffi #:ctype)
-  (:export #:ctype #:cthe #:clocally #:clet #:clet* #:csetf #:& #:-> #:[] #:foreign-alloca)
-  (:nicknames #:cops))
-
-(in-package #:cffi-ops)
