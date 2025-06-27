d8d53e55e90f63f6bf7d07ca2f2bc7a2bce918f2
Colin Woodbury <colin@fosskers.ca>
2025-06-28 06:58:33 +0900

    [game] thank TRD

--- game/main.lisp
+++ game/main.lisp
@@ -464,8 +464,8 @@ the player is pressing."
                    (- +world-max-x+ 136)
                    (- +world-max-y+ 20)
                    10)
-  (draw-white-text (format nil "Concept by JPJ")
-                   (- +world-max-x+ 82)
+  (draw-white-text (format nil "Concept by JPJ and TRD")
+                   (- +world-max-x+ 130)
                    (- +world-max-y+ 10)
                    10))
 
